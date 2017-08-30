bed824a244f1f4a82ae29ce56231f3dfe9789199
Anton Shubin <2spy4x@gmail.com>
2017-08-30 16:55:01 +0400

    test(_core/signed-in)#18: test signOut() and click on "Sign out" button

--- src/app/_core/signed-in/signed-in.component.spec.ts
+++ src/app/_core/signed-in/signed-in.component.spec.ts
@@ -2,10 +2,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignedInComponent } from './signed-in.component';
 import { AppTestingModule } from '../../appTesting.module';
+import { Store } from '@ngrx/store';
+import { AppState } from '../store/app.state';
+import { AuthSignOutAction } from '../store/auth/authSignOut.action';
 
 describe('SignedInComponent', () => {
   let component: SignedInComponent;
   let fixture: ComponentFixture<SignedInComponent>;
+  let store: Store<AppState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,6 +19,8 @@ describe('SignedInComponent', () => {
       ]
     })
     .compileComponents();
+    store = TestBed.get(Store);
+    spyOn(store, 'dispatch').and.callThrough();
   }));
 
   beforeEach(() => {
@@ -25,5 +31,17 @@ describe('SignedInComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch AuthSignOutAction on signOut()', () => {
+    component.signOut();
+    expect(store.dispatch).toHaveBeenCalledWith(
+      new AuthSignOutAction());
+  });
+
+  it('should dispatch AuthSignOutAction on click Sign Out button in menu()', () => {
+    fixture.debugElement.nativeElement.querySelector('.menu__item_sign-out').click();
+    expect(store.dispatch).toHaveBeenCalledWith(
+      new AuthSignOutAction());
   });
 });
