0a873496da5dbc23e18322b877d8906781bc53d0
Anton Shubin <2spy4x@gmail.com>
2017-09-07 17:49:25 +0300

    test(_core/store/auth)#18: cover "authSignOut.action" with tests

---
+++ src/app/_core/store/auth/authSignOut.action.spec.ts
@@ -0,0 +1,32 @@
+import {
+  AuthSignOutAction,
+  AuthSignOutActionHandler,
+  AuthSignOutActionType
+} from './authSignOut.action';
+import { AppStateInitial } from '../app.state';
+
+describe('authSignOut.action.ts', () => {
+  describe('Action', () => {
+    let action: AuthSignOutAction;
+    beforeEach(() => {
+      action = new AuthSignOutAction();
+    });
+
+    it('should have proper type', () => {
+      expect(action.type).toEqual(AuthSignOutActionType);
+    });
+
+    it('should have no payload', () => {
+      expect(action['payload']).toEqual(undefined);
+    });
+  });
+
+  describe('Handler', () => {
+    it('should return state to initial shape', () => {
+      expect(AuthSignOutActionHandler(
+        {core: {user: {name: 'Anton'}}},
+        new AuthSignOutAction()))
+        .toEqual(AppStateInitial);
+    });
+  });
+});
