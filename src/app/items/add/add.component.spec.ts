aae48055b3079809e1e1690e4d0e0619d780c01a
Anton Shubin <2spy4x@gmail.com>
2017-09-08 19:20:32 +0300

    test(items)#18: cover "add.component" with tests

--- src/app/items/add/add.component.spec.ts
+++ src/app/items/add/add.component.spec.ts
@@ -2,19 +2,27 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ItemsAddComponent } from './add.component';
 import { AppTestingModule } from '../../appTesting.module';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../_core/store/app.state';
+import { ItemsLoadedAction } from '../../_core/store/items/loaded/itemsLoaded.action';
+import { ItemsAddAction } from '../../_core/store/items/add/itemsAdd.action';
 
 describe('ItemsAddComponent', () => {
   let component: ItemsAddComponent;
   let fixture: ComponentFixture<ItemsAddComponent>;
+  let store: Store<AppState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ItemsAddComponent ],
+      declarations: [ItemsAddComponent],
       imports: [
         AppTestingModule
       ]
     })
-    .compileComponents();
+      .compileComponents();
+    store = TestBed.get(Store);
+    store.dispatch(new ItemsLoadedAction(['1', '2']));
+    spyOn(store, 'dispatch').and.callThrough();
   }));
 
   beforeEach(() => {
@@ -25,5 +33,11 @@ describe('ItemsAddComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch "ItemsAddAction" when "add(...)" is called', () => {
+    const newItem = 'newItem';
+    component.add(newItem);
+    expect(store.dispatch).toHaveBeenCalledWith(new ItemsAddAction(newItem));
   });
 });
