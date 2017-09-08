d8bd64e9b4b2bfc936761f8793803d3a5499ee49
Anton Shubin <2spy4x@gmail.com>
2017-09-08 17:46:34 +0300

    test(dashboard)#18: cover "dashboard.component" with tests

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -4,6 +4,11 @@ import { DashboardComponent } from './dashboard.component';
 import { Store } from '@ngrx/store';
 import { AuthSignInAction } from '../_core/store/auth/signIn/authSignIn.action';
 import { AppTestingModule } from '../appTesting.module';
+import { DashboardChangeChartTypeAction } from './_store/chartType/changeChartType/changeChartType.action';
+import { ItemsAddAction } from '../_core/store/items/add/itemsAdd.action';
+import { ItemsLoadedAction } from '../_core/store/items/loaded/itemsLoaded.action';
+import { TagsLoadedAction } from '../_core/store/tags/loaded/tagsLoaded.action';
+import { TagsAddAction } from '../_core/store/tags/add/tagsAdd.action';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -18,7 +23,7 @@ describe('DashboardComponent', () => {
     })
       .compileComponents();
     store = TestBed.get(Store);
-    // store.dispatch(new AuthSignInAction({name: 'AntonTest'}));
+    spyOn(store, 'dispatch').and.callThrough();
   }));
 
   beforeEach(() => {
@@ -29,5 +34,103 @@ describe('DashboardComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('chartType', () => {
+    let indicatorChartType: Element;
+    beforeEach(() => {
+      indicatorChartType = fixture.debugElement.nativeElement
+        .querySelector('.indicator_chart-type');
+    });
+
+    it('should change chart type when "changeChartType(...)" is called', () => {
+      component.changeChartType('line');
+      expect(store.dispatch)
+        .toHaveBeenCalledWith(new DashboardChangeChartTypeAction('line'));
+      fixture.detectChanges();
+      expect(indicatorChartType.innerHTML).toEqual('line');
+      component.changeChartType('pie');
+      expect(store.dispatch)
+        .toHaveBeenCalledWith(new DashboardChangeChartTypeAction('pie'));
+      fixture.detectChanges();
+      expect(indicatorChartType.innerHTML).toEqual('pie');
+    });
+
+    it('should change chart type when "chart-type-switcher" is clicked', () => {
+      fixture.debugElement.nativeElement
+        .querySelector('.chart-type-switcher_line').click();
+      expect(store.dispatch)
+        .toHaveBeenCalledWith(new DashboardChangeChartTypeAction('line'));
+      fixture.detectChanges();
+      expect(indicatorChartType.innerHTML).toEqual('line');
+      fixture.debugElement.nativeElement
+        .querySelector('.chart-type-switcher_pie').click();
+      expect(store.dispatch)
+        .toHaveBeenCalledWith(new DashboardChangeChartTypeAction('pie'));
+      fixture.detectChanges();
+      expect(indicatorChartType.innerHTML).toEqual('pie');
+    });
+  });
+
+  describe('items count', () => {
+    let indicatorItemsCount: Element;
+    beforeEach(() => {
+      indicatorItemsCount = fixture.debugElement.nativeElement
+        .querySelector('.indicator_items-count');
+      store.dispatch(new ItemsLoadedAction(['1', '2', '3', '4', '5']));
+    });
+
+    it('should update items count indicator when item added', () => {
+      fixture.detectChanges();
+      expect(indicatorItemsCount.innerHTML).toEqual('Items: 5');
+      store.dispatch(new ItemsAddAction('new Item 1'));
+      fixture.detectChanges();
+      expect(indicatorItemsCount.innerHTML).toEqual('Items: 6');
+      store.dispatch(new ItemsAddAction('new Item 2'));
+      fixture.detectChanges();
+      expect(indicatorItemsCount.innerHTML).toEqual('Items: 7');
+    });
+
+    it('should update items count indicator when items loaded', () => {
+      fixture.detectChanges();
+      expect(indicatorItemsCount.innerHTML).toEqual('Items: 5');
+      store.dispatch(new ItemsAddAction('new Item 1'));
+      fixture.detectChanges();
+      expect(indicatorItemsCount.innerHTML).toEqual('Items: 6');
+      store.dispatch(new ItemsLoadedAction(['1', '2']));
+      fixture.detectChanges();
+      expect(indicatorItemsCount.innerHTML).toEqual('Items: 2');
+    });
+  });
+
+  describe('tags count', () => {
+    let indicatorTagsCount: Element;
+    beforeEach(() => {
+      indicatorTagsCount = fixture.debugElement.nativeElement
+        .querySelector('.indicator_tags-count');
+      store.dispatch(new TagsLoadedAction(['1', '2', '3', '4', '5']));
+    });
+
+    it('should update tags count indicator when item added', () => {
+      fixture.detectChanges();
+      expect(indicatorTagsCount.innerHTML).toEqual('Tags: 5');
+      store.dispatch(new TagsAddAction('new Tag 1'));
+      fixture.detectChanges();
+      expect(indicatorTagsCount.innerHTML).toEqual('Tags: 6');
+      store.dispatch(new TagsAddAction('new Tag 2'));
+      fixture.detectChanges();
+      expect(indicatorTagsCount.innerHTML).toEqual('Tags: 7');
+    });
+
+    it('should update tags count indicator when tags loaded', () => {
+      fixture.detectChanges();
+      expect(indicatorTagsCount.innerHTML).toEqual('Tags: 5');
+      store.dispatch(new TagsAddAction('new Tag 1'));
+      fixture.detectChanges();
+      expect(indicatorTagsCount.innerHTML).toEqual('Tags: 6');
+      store.dispatch(new TagsLoadedAction(['1', '2']));
+      fixture.detectChanges();
+      expect(indicatorTagsCount.innerHTML).toEqual('Tags: 2');
+    });
   });
 });
