745685dedb443c649a26959c0cd3a8300ca3a3c1
Anton Shubin <2spy4x@gmail.com>
2017-09-08 19:35:12 +0300

    test(tags)#18: cover "add.component" with tests

--- src/app/tags/add/add.component.spec.ts
+++ src/app/tags/add/add.component.spec.ts
@@ -2,10 +2,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TagsAddComponent } from './add.component';
 import { AppTestingModule } from '../../appTesting.module';
+import { AppState } from '../../_core/store/app.state';
+import { Store } from '@ngrx/store';
+import { TagsLoadedAction } from '../../_core/store/tags/loaded/tagsLoaded.action';
+import { TagsAddAction } from '../../_core/store/tags/add/tagsAdd.action';
 
 describe('TagsAddComponent', () => {
   let component: TagsAddComponent;
   let fixture: ComponentFixture<TagsAddComponent>;
+  let store: Store<AppState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -15,6 +20,9 @@ describe('TagsAddComponent', () => {
       ]
     })
     .compileComponents();
+    store = TestBed.get(Store);
+    store.dispatch(new TagsLoadedAction(['1', '2']));
+    spyOn(store, 'dispatch').and.callThrough();
   }));
 
   beforeEach(() => {
@@ -25,5 +33,11 @@ describe('TagsAddComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch "TagsAddAction" when "add(...)" is called', () => {
+    const newTag = 'newTag';
+    component.add(newTag);
+    expect(store.dispatch).toHaveBeenCalledWith(new TagsAddAction(newTag));
   });
 });
