92f56165e419b89272e946b40738b909b007ae61
Anton Shubin <2spy4x@gmail.com>
2017-09-08 16:13:47 +0300

    test(dashboard/_store/chartType)#18: cover "changeChartType.action" with tests

---
+++ src/app/dashboard/_store/chartType/changeChartType/changeChartType.action.spec.ts
@@ -0,0 +1,37 @@
+import {
+  DashboardChangeChartTypeAction,
+  DashboardChangeChartTypeActionHandler,
+  DashboardChangeChartTypeActionType
+} from './changeChartType.action';
+import { DashboardChartType } from '../chartType.type';
+import { DashboardState } from '../../dashboard.state';
+
+describe('changeChartType.action.ts', () => {
+  describe('Action', () => {
+    let action: DashboardChangeChartTypeAction;
+    beforeEach(() => {
+      action = new DashboardChangeChartTypeAction('pie' as DashboardChartType);
+    });
+
+    it('should have proper type', () => {
+      expect(action.type).toEqual(DashboardChangeChartTypeActionType);
+    });
+
+    it('should have proper payload', () => {
+      expect(action.payload).toEqual('pie');
+    });
+  });
+
+  describe('Handler', () => {
+    it('should change chartType from "pie" to "line"', () => {
+      const newState = DashboardChangeChartTypeActionHandler({chartType: 'pie'},
+        new DashboardChangeChartTypeAction('line'));
+      expect(newState).toEqual({chartType: 'line'});
+    });
+    it('should change chartType from "line" to "pie"', () => {
+      const newState = DashboardChangeChartTypeActionHandler({chartType: 'line'},
+        new DashboardChangeChartTypeAction('pie'));
+      expect(newState).toEqual({chartType: 'pie'});
+    });
+  });
+});
