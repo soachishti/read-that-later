2728d0a70306d8d4bd1150b969a49dd5962a0030
Anton Shubin <2spy4x@gmail.com>
2017-09-21 14:07:34 +0300

    feat(_core/store/auth/signedIn)#25: sign out on !user returned by firebase auth
    Closes #25

--- src/app/_shared/auth/auth.service.ts
+++ src/app/_shared/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import { AuthSignedInAction } from '../../_core/store/auth/signedIn/authSignedIn.action';
 import { pick } from 'lodash';
+import { AuthSignOutAction } from '../../_core/store/auth/signOut/authSignOut.action';
 
 @Injectable()
 export class AuthService {
@@ -18,10 +19,9 @@ export class AuthService {
             'providerId', 'uid'];
           const userInfo = pick<firebase.UserInfo, firebase.User>(user, fields);
           this.store.dispatch(new AuthSignedInAction(userInfo));
+        } else {
+          this.store.dispatch(new AuthSignOutAction());
         }
-        /*else {
-         this.store.dispatch(new AuthSignOutAction());
-         }*/
       });
   }
 
